b4ac7955464f068295b6280efbe2aec53d9abae5
Erick Njuki <njukierick@gmail.com>
2021-12-30 12:52:49 +0300

    webpack changes on entry file

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        publicPath: 'dist'
-    },
-    stats: 'errors-only',
-    devServer: {
-        contentBase: './',
-        hot: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+  entry: "./src/App.js",
+  output: {
+    filename: "bundle.js",
+    publicPath: "dist",
+  },
+  stats: "errors-only",
+  devServer: {
+    contentBase: "./",
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
